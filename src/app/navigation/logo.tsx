0c859215b7358dd55480fd2822c0126a17b42b4f
andylaw <974088545@qq.com>
2023-06-22 08:29:31 +0800

    feat: update logo's style

--- src/app/navigation/logo.tsx
+++ src/app/navigation/logo.tsx
@@ -1,3 +1,12 @@
-export default function Logo(){
-    return <span>Logo here</span>
+export default function Logo() {
+    return <div style={{fontSize: '20px',userSelect:'none'}}>
+        <span style={{color: '#1e9bd3'}}>前端</span>
+        &nbsp;
+        <span>.</span>
+        <span style={{
+            backgroundImage: '-webkit-linear-gradient(left, #a504f1, #edc03c)',
+            '-webkit-background-clip': 'text',
+            '-webkit-text-fill-color': 'transparent'
+        }}>QA</span>
+    </div>
 }
