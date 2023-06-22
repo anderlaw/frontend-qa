a737aa71b5a159431f1f6dc98b59b2c34943cc6e
andylaw <974088545@qq.com>
2023-06-22 08:42:57 +0800

    feat: update navigation's style

--- src/app/navigation/index.tsx
+++ src/app/navigation/index.tsx
@@ -2,23 +2,25 @@ import Logo from "@/app/navigation/logo";
 
 export default function NavigationBar() {
     return (
-        <nav style={{
-            backgroundColor: 'grey',
-            display:'flex',
-            alignItems:'center'
+        <nav className="main-nav-container" style={{
+            backgroundColor: '#1a1a1b',
+            display: 'flex',
+            alignItems: 'center',
+            color: '#cecdcd',
+            userSelect:'none'
         }}>
             <Logo/>
-            <ol style={{display: 'flex', height: '60px', lineHeight: '60px',marginLeft:'200px'}}>
+            <ol style={{display: 'flex', height: '60px', lineHeight: '60px', marginLeft: '200px'}}>
                 <li>
-                    <a href="/html" style={{display: 'inline-block'}}>HTML</a>
+                    <a href="/html">HTML</a>
                 </li>
-                <li style={{marginLeft: '40px'}}>
+                <li>
                     <a href="/css">CSS</a>
                 </li>
-                <li style={{marginLeft: '40px'}}>
+                <li>
                     <a href="/javascript">JavaScript</a>
                 </li>
-                <li style={{marginLeft: '40px'}}>
+                <li>
                     <a href="/framework">框架</a>
                 </li>
             </ol>
