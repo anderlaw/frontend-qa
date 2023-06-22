16b622a63375f3c6de1e485c2b4bb4169bc98185
andylaw <974088545@qq.com>
2023-06-22 22:13:34 +0800

    chore: define some fake data

--- src/data/html/index.ts
+++ src/data/html/index.ts
@@ -1,11 +1,11 @@
-interface QAData {
-    title: string
-    description?: string
+export interface IQAData {
+    question: string
+    tips?: string
     answer: string
 }
-const HTMLData: QAData [] = [
+const HTMLData: IQAData [] = [
     {
-        title: 'HTML语义化是什么意思',
+        question: 'HTML语义化是什么意思',
         answer:'HTML语义化指赋予HTML标签一定的语法结构意义，这种意义除了在开发时有一定的意义，避免一些标签的滥用如一些早期的网站中使用div+span布局开发整个网站，还给搜索引擎提供帮助更好地理解网站的内容'
     }
 ]
