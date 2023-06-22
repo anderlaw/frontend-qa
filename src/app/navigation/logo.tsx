256a72e69e9df2efd1805cda9c5c8d94b172b810
andylaw <974088545@qq.com>
2023-06-22 22:03:11 +0800

    chore: 修复—webkit-前缀在react style绑定中的写法

--- src/app/navigation/logo.tsx
+++ src/app/navigation/logo.tsx
@@ -1,12 +1,12 @@
 export default function Logo() {
-    return <div style={{fontSize: '20px',userSelect:'none'}}>
+    return <div style={{fontSize: '20px',userSelect:'none',padding:'0 20px'}}>
         <span style={{color: '#1e9bd3'}}>前端</span>
         &nbsp;
         <span>.</span>
         <span style={{
             backgroundImage: '-webkit-linear-gradient(left, #a504f1, #edc03c)',
-            '-webkit-background-clip': 'text',
-            '-webkit-text-fill-color': 'transparent'
+            WebkitBackgroundClip:'text',
+            WebkitTextFillColor:'transparent'
         }}>QA</span>
     </div>
 }
